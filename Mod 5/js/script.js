f8c025f9ffedc23baae8bb5dc5ebf056432aef38
Vineeth-Reddy1811 <64138602+Vineeth-Reddy1811@users.noreply.github.com>
2020-05-31 20:54:39 +0530

    Update script.js

--- Mod 5/js/script.js
+++ Mod 5/js/script.js
@@ -80,18 +80,15 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 // *** start ***
 // On first load, show home view
+showLoading("#main-content");
 
-  showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
   buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitely setting the flag to get JSON from server processed into an object literal
+  true); // Explicitly setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
-
-// Builds HTML for the home page based on categories array
-// returned from the server.
 function buildAndShowHomeHTML (categories) {
 
   // Load home snippet page
@@ -132,16 +129,8 @@ function buildAndShowHomeHTML (categories) {
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  // Given array of category objects, returns a random category object.
+
+// Given array of category objects, returns a random category object.
 function chooseRandomCategory (categories) {
   // Choose a random index into the array (from 0 inclusively until array length (exclusively))
   var randomArrayIndex = Math.floor(Math.random() * categories.length);
